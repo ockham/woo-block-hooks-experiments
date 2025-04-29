f83e6ea6a28e1f38c049b0a60472a481925d0414
Bernie Reiter <ockham@raz.or.at>
2025-04-29 16:47:49 +0200

    Experiment: Try automatic infering of wrapper blocks

--- woo-block-hooks-experiments.php
+++ woo-block-hooks-experiments.php
@@ -28,6 +28,34 @@ add_filter(
 	4
 );
 
+function set_block_parent( $metadata ) {
+    // Only add the parent to our test block type.
+    if ( isset( $metadata['name'] ) && 'core/paragraph' === $metadata['name'] ) {
+		$metadata['parent'] = 'woocommerce/accordion-panel';
+    }
+	return $metadata;
+};
+add_filter( 'block_type_metadata', 'set_block_parent' );
+
+function find_block_wrapper_path( $ancestor, $descendant ) {
+	$ancestor_block_type_definition = WP_Block_Type_Registry::get_instance()->get_registered( $ancestor );
+	$allowed_blocks = $ancestor_block_type_definition->allowed_blocks ?? array();
+	if ( empty( $allowed_blocks ) || in_array( $descendant, $allowed_blocks, true ) ) {
+		// Check if the descendant also lists the ancestor as its parent.
+		$descendant_block_type_definition = WP_Block_Type_Registry::get_instance()->get_registered( $descendant );
+		if ( ! empty( $descendant_block_type_definition->parent ) && $ancestor === $descendant_block_type_definition->parent ) {
+			return array( $descendant);
+		}
+	}
+
+	foreach ( $allowed_blocks as $allowed_block ) {
+		$path = find_block_wrapper_path( $allowed_block, $descendant );
+		if ( ! empty( $path ) ) {
+			return array_merge( array( $allowed_block ), $path );
+		}
+	}
+}
+
 add_filter(
 	'hooked_block_core/paragraph',
 	function (
@@ -47,15 +75,18 @@ add_filter(
 		if ( 'woocommerce/accordion-group' === $parsed_anchor_block['blockName'] && $relative_position === 'last_child' ) {
 			$parsed_hooked_block['innerContent'] = array( '<p>Hello World!</p>' );
 
-			// We wrap our hooked paragraph block in an Accordion Panel block inside of an Accordion Item block.
-			$parsed_hooked_block = create_accordion_item_block(
-				array(), // Attributes for Accordion Item block.
-				array(
-					// Inner blocks of the Accordion Item block: Header and Panel.
-					create_accordion_header_block( array( 'title' => 'Accordion Header block' ) ),
-					create_accordion_panel_block( array(), array( $parsed_hooked_block ) )
-				)
-			);
+			$path = find_block_wrapper_path( $parsed_anchor_block['blockName'], $hooked_block_type );
+
+			$wrapper = $parsed_hooked_block;
+			array_pop( $path );
+			foreach( array_reverse( $path ) as $block ) {
+				$wrapper_block_function = 'create_' . str_replace( array( '/', '-' ) , '_', $block ) . '_block';
+				if ( function_exists( $wrapper_block_function ) ) {
+					$wrapper = call_user_func( $wrapper_block_function, array(), array( $wrapper ) );
+				}
+			}
+
+			$parsed_hooked_block = $wrapper;
 		}
 
 		return $parsed_hooked_block;
